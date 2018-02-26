e1e13182e91cb779300c7bc4322fd483e59ce22f
Eugene Moninets <darkstarmacho@gmail.com>
2018-02-26 10:49:27 +0200

    Add two extension methods

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -59,5 +59,25 @@ namespace LanguageFeatures.Controllers
 
             return View("Result", (object)stringArray[1]);
         }
+
+        public ViewResult UseExtension()
+        {
+            //Create and populate ShoppingCart
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name = "Kayak", Price = 275M},
+                    new Product {Name = "Lifejacket", Price = 48.95M},
+                    new Product {Name = "Soccer ball", Price = 19.50M},
+                    new Product {Name = "Corner flag", Price = 34.95M}
+                }
+            };
+
+            //Get the total value of the products in the cart
+            decimal cartTotal = cart.TotalPrices();
+
+            return View("Result", (object)String.Format("Total: {0:c}", cartTotal));
+        }
     }
 }
--- LanguageFeatures/LanguageFeatures/Models/ShoppingCart.cs
+++ LanguageFeatures/LanguageFeatures/Models/ShoppingCart.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
 namespace LanguageFeatures.Models
 {
     public class ShoppingCart
     {
+        public List<Product> Products { get; set; }
     }
 }

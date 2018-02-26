102e5f603ce54a91299518abf2e6a5d9064f1798
Eugene Moninets <darkstarmacho@gmail.com>
2018-02-26 16:35:30 +0200

    Add an async method which shows a content's length of the page

---
+++ LanguageFeatures/LanguageFeatures/Models/MyAsyncMethods.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace LanguageFeatures.Models
+{
+    public class MyAsyncMethods
+    {
+        public async static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+
+            var httpMessage = await client.GetAsync("http://apress.com");
+
+            //We could do other things while we are waiting 
+            //for the HTTP request to complete
+            return httpMessage.Content.Headers.ContentLength;            
+        }
+    }
+}

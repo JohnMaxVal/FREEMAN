6dfbaf3d0b6be769148ff37b10a8519854560de6
Eugene Moninets <darkstarmacho@gmail.com>
2018-02-26 09:55:17 +0200

    Add some features with futuring modifications

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
-    {
-        // GET: Home
-        public ActionResult Index()
+    {        
+        public string Index()
         {
-            return View();
+            return "Navigate to a URL to show an example";
+        }
+
+        public ActionResult AutoProperty()
+        {
+            //Create a new product object
+            Product myProduct = new Product();
+
+            //Set the property value
+            myProduct.Name = "Kayak";
+
+            //Get the property
+            string productName = myProduct.Name;
+
+            //Generate the view
+            return View("Result", (object)String.Format("Product name: {0}", productName));
         }
     }
 }

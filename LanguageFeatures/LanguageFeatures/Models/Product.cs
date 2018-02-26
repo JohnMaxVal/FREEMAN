eb7a10c437f4db8797b63ad1670c2a7d0e5709ba
Eugene Moninets <darkstarmacho@gmail.com>
2018-02-26 10:00:34 +0200

    Modify Product.cs class by adding some additional auto properties

--- LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -9,10 +9,16 @@ namespace LanguageFeatures.Models
     {
         private string name;
 
+        public int ProductID { get; set; }
+
         public string Name
         {
-            get { return name; }
+            get { return ProductID + name; }
             set { name = value; }
         }
+
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
     }
 }

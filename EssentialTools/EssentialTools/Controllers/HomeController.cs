f2fcbca9a47afcf862ea79069cc6b3d8ef0c7307
Eugene Moninets <darkstarmacho@gmail.com>
2018-02-27 11:30:55 +0200

    Add an Index.cshtml file

---
+++ EssentialTools/EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EssentialTools.Models;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product[] products =
+        {
+            new Product {Name = "Kayak", Price = 275m},
+            new Product {Name="Lifejacket", Price=48.95m},
+            new Product {Name = "Soccer ball", Price = 19.50M},
+            new Product {Name = "Corner flag", Price = 34.95M}
+        };
+
+        public ActionResult Index()
+        {
+            LinqValueCalculator calc = new LinqValueCalculator();
+
+            ShoppingCart cart = new ShoppingCart(calc)
+            {
+                Products = products
+            };
+
+            decimal totalValue = cart.CalculateProductTotal();
+
+            return View(totalValue);
+        }
+    }
+}

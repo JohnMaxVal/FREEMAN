ec3aecbb4f304b9ca7486f75aa63b56a07f7a94d
Eugen Moninets <darkstarmacho@gmail.com>
2018-02-28 08:07:33 +0200

    Add a dependeciAdd a dependecies

--- EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
+++ EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -29,6 +29,7 @@ namespace EssentialTools.Infrastructure
         private void AddBindings()
         {
             kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            kernel.Bind<IDiscountHepler>().To<DefaultDiscountHelper>();
         }
     }
 }
---
+++ EssentialTools/EssentialTools/Models/Discount.cs
@@ -0,0 +1,15 @@
+﻿namespace EssentialTools.Models
+{
+    public interface IDiscountHepler
+    {
+        decimal ApplyDiscount(decimal totalParam);
+    }
+
+    public class DefaultDiscountHelper : IDiscountHepler
+    {
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            return (totalParam - (10m / 100m * totalParam));
+        }
+    }
+}

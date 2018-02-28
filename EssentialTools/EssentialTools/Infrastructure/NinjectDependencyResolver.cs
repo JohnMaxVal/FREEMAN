f3c2b64aa72e31e194d22a35d7e170b8f20a5ef5
Eugene Moninets <darkstarmacho@gmail.com>
2018-02-28 11:52:21 +0200

    Specifying Property and Constructor Parameter Values

--- EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
+++ EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -29,7 +29,7 @@ namespace EssentialTools.Infrastructure
         private void AddBindings()
         {
             kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
-            //kernel.Bind<IDiscountHepler>().To<DefaultDiscountHelper>();
+            kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>().WithConstructorArgument("discountParam", 50m);
         }
     }
 }

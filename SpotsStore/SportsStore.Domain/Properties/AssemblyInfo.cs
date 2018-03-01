03140bd02a8bb6bcf7837ce5d83b8eb699714a09
Eugen Moninets <darkstarmacho@gmail.com>
2018-03-01 07:17:24 +0200

    Install all the packages

--- SpotsStore/SportsStore.Domain/Properties/AssemblyInfo.cs
+++ SpotsStore/SportsStore.Domain/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("16cdf416-8f07-4e36-b615-6528e3f48263")]
+[assembly: Guid("8c95071e-b779-47ed-8ee5-8a32a6637664")]
 
 // Version information for an assembly consists of the following four values:
 //

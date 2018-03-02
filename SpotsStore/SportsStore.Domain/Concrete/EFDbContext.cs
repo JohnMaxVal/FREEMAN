db87c94634cb7039438a681c8c4ceb08dcc3a1c6
Eugene Moninets <darkstarmacho@gmail.com>
2018-03-02 15:32:29 +0200

    Create ProductRepository

--- SpotsStore/SportsStore.Domain/Concrete/EFDbContext.cs
+++ SpotsStore/SportsStore.Domain/Concrete/EFDbContext.cs
@@ -3,6 +3,8 @@ using System.Data.Entity;
 
 namespace SportsStore.Domain.Concrete
 {
+    //context class
+
     class EFDbContext : DbContext
     {
         public DbSet<Product> Products { get; set; }

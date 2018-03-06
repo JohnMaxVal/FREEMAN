506a6d8ae8c474080750a08031a6cc11e5efd21d
Eugene Moninets <darkstarmacho@gmail.com>
2018-03-06 19:33:57 +0200

    Chapter 10 is done

--- SpotsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SpotsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
+using System.Configuration;
 using Ninject;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Concrete;
@@ -30,6 +31,13 @@ namespace SportsStore.WebUI.Infrastructure
         private void AddBindings()
         {            
             kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager.AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>().WithConstructorArgument("settings", emailSettings);
         }
     }
 }

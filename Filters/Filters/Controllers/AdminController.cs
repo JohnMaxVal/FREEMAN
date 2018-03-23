4fb6da483abf5ae720e75201f285afba46d6c6a2
Eugene Moninets <darkstarmacho@gmail.com>
2018-03-23 18:44:11 +0200

    510

---
+++ Filters/Filters/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Filters.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        public ViewResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ Filters/Filters/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Filters.Infrastructure;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize(Users ="admin")]
+        public string Index()
+        {
+            return "This is an Index method on the Home Controller";
+        }
+        
+        [GoogleAuth]
+        [Authorize(Users = "bob@google.com")]
+        public string List()
+        {
+            return "This is a List action on the Home Controller";
+        }
+
+        [HandleError(ExceptionType =typeof(ArgumentOutOfRangeException), View ="RangeError")]
+        public string RangeTest(int id)
+        {
+            if(id > 100)
+            {
+                return string.Format("The id value is: {0}", id);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("id", id, "");
+            }
+        }
+    }
+}

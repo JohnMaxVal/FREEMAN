da43d180a90ee5324952ffed406c1db295dd7935
Eugen Moninets <darkstarmacho@gmail.com>
2018-04-18 09:09:44 +0300

    Chapter 22

---
+++ HelperMethods/HelperMethods/Models/Metadata/PersonMetadata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelperMethods.Models
+{
+    [MetadataType(typeof(PersonMetadata))]
+    public partial class PersonMetadata
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Address HomeAddress { get; set; }
+        public bool IsApproved { get; set; }
+        public Role Role { get; set; }
+    }
+}

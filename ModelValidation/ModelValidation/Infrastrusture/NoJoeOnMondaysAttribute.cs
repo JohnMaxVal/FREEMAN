7fde5a9077536778ab21737556d99727f60a7209
Eugen Moninets <darkstarmacho@gmail.com>
2018-04-19 23:51:52 +0300

    Chapter 25

---
+++ ModelValidation/ModelValidation/Infrastrusture/NoJoeOnMondaysAttribute.cs
@@ -0,0 +1,30 @@
+﻿using ModelValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModelValidation.Infrastrusture
+{
+    public class NoJoeOnMondaysAttribute : ValidationAttribute
+    {
+        public NoJoeOnMondaysAttribute()
+        {
+            ErrorMessage = "Joe cannot bool appointments on Mondays";
+        }
+
+        public override bool IsValid(object value)
+        {
+            Appointment appt = value as Appointment;
+            if (appt == null || string.IsNullOrEmpty(appt.ClientName) || appt.Date > DateTime.Now)
+            {
+                return true;
+            }
+            else
+            {
+                return !(appt.ClientName == "Joe" && appt.Date.DayOfWeek == DayOfWeek.Monday);
+            }
+        }
+    }
+}
---
+++ ModelValidation/ModelValidation/Models/Appointment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using ModelValidation.Infrastrusture;
+using System.Web.Mvc;
+
+namespace ModelValidation.Models
+{
+    public class Appointment
+    {
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
+        public string ClientName { get; set; }
+
+        [DataType(DataType.Date)]
+        [Remote("ValidateDate", "Home")]
+        public DateTime Date { get; set; }
+        
+        public bool TermsAccepted { get; set; }
+    }
+}

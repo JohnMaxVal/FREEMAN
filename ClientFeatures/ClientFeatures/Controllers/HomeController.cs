10caf5be02d301901f37a5e925d5353cdc9c5bfd
Eugen Moninets <darkstarmacho@gmail.com>
2018-04-20 17:08:19 +0300

    Chapter 26-27

---
+++ ClientFeatures/ClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClientFeatures.Models;
+
+namespace ClientFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment
+            {
+                ClientName = "Eugene",
+                TermAccepted = true
+            });
+        }
+
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment appt)
+        {
+            return Json(appt, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
